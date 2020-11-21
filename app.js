c2a94355001e7434ea8f08c649e0bceb0617a123
wschiang <sinon049@yahoo.com.tw>
2020-11-21 20:02:05 +0800

    refactor: delete unused command

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const exphb = require('express-handlebars')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 
 app.engine('hbs', exphb({ default: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(port, (req, res) => {
